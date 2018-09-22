194d47692c15181d8ca0e523386d0714cb56f228
Aahana Bernard <monica.aahana@gmail.com>
2018-09-22 16:46:31 -0400

    Memory calculation
    Added the asizeof for memory utilization for different n-bit values.

---
+++ multiply_final.py
@@ -0,0 +1,91 @@
+ import timeit, random
+
+class Solution:
+    def __init__(self, bits):
+        self.bits = bits
+        self.MAX_INT = int(pow(2, self.bits*2)/2)-1
+        self.MIN_INT = int(pow(2, self.bits*2)/2)
+        self.MASK = pow(2, bits*2)
+
+    def generateRandomNum(self):
+        return random.randrange(pow(-2, self.bits-1), pow(2, self.bits-1))
+
+    def add(self, x, y):
+        # Iterate till there is no carry
+        while (y != 0):
+            # carry now contains common
+            # set bits of x and y
+            carry = x & y
+            #print("x:", x, "y:", y, "carry:", carry)
+
+            # Exclusive OR
+            # Sum of bits of x and y where at
+            # least one of the bits is not set
+            x = (x ^ y) % self.MASK
+
+            # Carry is shifted by one so that   
+            # adding it to x gives the required sum
+            y = (carry << 1) % self.MASK
+
+        return x if x <= self.MAX_INT else ~((x % self.MIN_INT) ^ self.MAX_INT) # to get rid of leading 1s for signed bit
+        #If x > MAX_INT then take the mod of x with MIN_INT and xor with MAX_INT and negate to get the answer in x.
+
+    def multiply(self, x,y):
+        res = 0;
+        while (y != 0):
+            
+            if (y & 1):
+                res = self.add(res, x) # if y is odd, add x to result
+
+            x = (x << 1) % self.MASK
+            y = (y >> 1) % self.MASK
+        return res
+        
+# We find the time and memory utilization for the test code. 
+TEST_CODE = '''
+
+for i, (num1, num2) in enumerate(zip(num1_list, num2_list)):
+    result[i] = calc.multiply(num1, num2)
+    #Answer check:
+    if result[i] != (num1*num2):
+        print("The multiplication of " + str(num1) + " and " + str(num2) + " is : " + str(result[i]), end=" ")
+        print("FALSE!!!")
+
+print("Bits:", bits, "\b, Size:", asizeof.asizeof(result), "bytes", end = ", ") 
+'''
+
+# setup code is not timed 
+SETUP_CODE = '''
+
+from __main__ import Solution
+
+# To install library: pip3 install pympler
+from pympler import asizeof # for memory utilization readings.
+
+bits = ***
+
+calc = Solution(bits) # Calc is an object for the class Solution. 
+
+# empty lists to store input values.
+num1_list = []
+num2_list = []
+
+for i in range(1000):
+    num1_list.append(calc.generateRandomNum())
+    num2_list.append(calc.generateRandomNum())
+
+# Create an array with all the values = 0 to store results 
+result = [0 for x in range(1000)]
+
+'''
+
+bitSizeArray = [4, 8, 16, 32, 64, 128, 256, 512]
+
+for j, bitSize in enumerate(bitSizeArray):
+    time = timeit.timeit(setup=SETUP_CODE.replace("***", str(bitSize)), stmt=TEST_CODE, number=1)
+    time = time * 1000 # convert time to milli seconds for easier readings.
+    print("Time:", "%.4f" % time, "ms") # limiting to 4 decimal places for time.
+
+
+
+
--- timeit_multiply.py
+++ timeit_multiply.py
@@ -1,4 +1,5 @@
 import timeit, random
+from memory_profiler import memory_usage
 
 class Solution:
     def __init__(self, bits):
@@ -11,11 +12,12 @@ class Solution:
         return random.randrange(pow(-2, self.bits-1), pow(2, self.bits-1))
 
     def add(self, x, y):
-        # Iterate till there is no carry 
+        # Iterate till there is no carry
         while (y != 0):
             # carry now contains common
             # set bits of x and y
             carry = x & y
+            #print("x:", x, "y:", y, "carry:", carry)
 
             # Exclusive OR
             # Sum of bits of x and y where at
@@ -32,14 +34,14 @@ class Solution:
     def multiply(self, x,y):
         res = 0;
         while (y != 0):
-            #print("x:", x, "y:", y, end=" ")
+            
             if (y & 1):
-                res = self.add(res, x); # if y is odd, add x to result
-            #print("res:", res)
+                res = self.add(res, x) # if y is odd, add x to result
+
             x = (x << 1) % self.MASK
             y = (y >> 1) % self.MASK
-        return res;
-
+        return res
+        
 TEST_CODE = '''
 
 num1 = calc.generateRandomNum()
@@ -59,12 +61,26 @@ calc = Solution(bits = ***) #Calc is an object for the class Solution.
 
 '''
 
-bitSizeArray = [8, 16, 32, 64, 128, 256, 512]
+# bitSizeArray = [4, 8, 16, 32, 64, 128, 256, 512]
 
 
-for bitSize in bitSizeArray:
-    print(str(bitSize)+": ", timeit.timeit(setup=SETUP_CODE.replace("***", str(bitSize)), stmt=TEST_CODE, number=1000))
+# for bitSize in bitSizeArray:
+#     print(str(bitSize)+": ", timeit.timeit(setup=SETUP_CODE.replace("***", str(bitSize)), stmt=TEST_CODE, number=1000))
+
+# print(timeit.timeit(setup=SETUP_CODE.replace("***", "16"), stmt=TEST_CODE, number=1000))
+
+def mainApp():
+    for i in range(1000):
+        calc = Solution(bits = 8) #Calc is an object for the class Solution. 
+        num1 = calc.generateRandomNum()
+        num2 = calc.generateRandomNum()
+        result = calc.multiply(num1, num2)
+        #Answer check:
+        if result != (num1*num2):
+            print("The multiplication of " + str(num1) + " and " + str(num2) + " is : " + str(result), end=" ")
+            print("FALSE!!!")
 
 
-
-#print(num1, num2, calc.multiply(num1, num2))
+mem_usage = memory_usage(mainApp, interval=0.001)
+# print('Memory usage (in chunks of 0.001 seconds): %s' % mem_usage)
+print('Maximum memory usage: %s' % max(mem_usage))
